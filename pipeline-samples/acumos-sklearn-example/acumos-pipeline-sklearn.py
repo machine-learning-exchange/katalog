fba0c5e2510c10edcdb05a0e5c53332fc27f602f
tomcli <tommy.chaoping.li@ibm.com>
2021-05-20 10:03:49 -0700

    add acumos pipeline example for sklearn models
    Signed-off-by: tomcli <tommy.chaoping.li@ibm.com>

---
+++ pipeline-samples/acumos-sklearn-example/acumos-pipeline-sklearn.py
@@ -0,0 +1,49 @@
+# Copyright 2021 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from kfp import dsl
+
+
+def convert_to_acumos_op():
+    return dsl.ContainerOp(
+        name='convert-models-to-acumos',
+        image='tomcli/acumos-model-convert:latest',
+        command=['python'],
+        arguments=['-u', 'sample.py']
+    )
+
+
+def echo_op(name="echo", text="placeholder"):
+    return dsl.ContainerOp(
+        name=name,
+        image='busybox',
+        command=['sh', '-c'],
+        arguments=['echo "%s"' % text]
+    )
+
+
+@dsl.pipeline(
+    name='Onboard MLX models to Acumos Marketplace',
+    description='A pipeline for onboarding MLX models to Acumos Marketplace'
+)
+def acumos_pipeline(
+):
+    pull_metadata = echo_op(name="process-model-source-files")
+    convert_to_acumos = convert_to_acumos_op().after(pull_metadata)
+    onboard_model = echo_op(name="onboard-acumos-models").after(convert_to_acumos)
+
+
+if __name__ == '__main__':
+    from kfp_tekton.compiler import TektonCompiler
+    TektonCompiler().compile(acumos_pipeline, __file__.replace('.py', '.yaml'))
---
+++ pipeline-samples/acumos-sklearn-example/components/convert_to_acumos/src/sample.py
@@ -0,0 +1,92 @@
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+"""
+Provides a scikit-learn Acumos model example
+"""
+import numpy as np
+import os
+
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+from acumos.modeling import Model, List, create_namedtuple
+from acumos.session import AcumosSession
+from minio import Minio
+
+data_bucket = os.getenv('DATA_BUCKET', 'mlpipeline')
+
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+clf = RandomForestClassifier(random_state=0)
+clf.fit(X, y)
+
+IrisDataFrame = create_namedtuple('IrisDataFrame', [('sepal_length', List[float]),
+                                                    ('sepal_width', List[float]),
+                                                    ('petal_length', List[float]),
+                                                    ('petal_width', List[float])])
+
+# =============================================================================
+# # starting in Python 3.6, you can alternatively use this simpler syntax:
+#
+# from acumos.modeling import NamedTuple
+#
+# class IrisDataFrame(NamedTuple):
+#     '''DataFrame corresponding to the Iris dataset'''
+#     sepal_length: List[float]
+#     sepal_width: List[float]
+#     petal_length: List[float]
+#     petal_width: List[float]
+# =============================================================================
+
+# =============================================================================
+# # A pandas DataFrame can also be used to infer appropriate NamedTuple types:
+#
+# import pandas as pd
+# from acumos.modeling import create_dataframe
+#
+# X_df = pd.DataFrame(X, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+# IrisDataFrame = create_dataframe('IrisDataFrame', X_df)
+# =============================================================================
+
+
+def classify_iris(df: IrisDataFrame) -> List[int]:
+    '''Returns an array of iris classifications'''
+    X = np.column_stack(df)
+    return clf.predict(X)
+
+
+model = Model(classify=classify_iris)
+
+session = AcumosSession()
+session.dump(model, 'my-iris', '.')  # creates ./my-iris
+
+# Store Acumos files to S3
+cos = Minio(os.getenv('S3_ENDPOINT', 'minio-service:9000'),
+            access_key=os.getenv('S3_ACCESS_KEY', 'minio'),
+            secret_key=os.getenv('S3_SECRET_KEY', 'minio123'),
+            secure=False)
+
+
+cos.fput_object(data_bucket, 'my-iris/metadata.json', 'my-iris/metadata.json')
+cos.fput_object(data_bucket, 'my-iris/model.proto', 'my-iris/model.proto')
+cos.fput_object(data_bucket, 'my-iris/model.zip', 'my-iris/model.zip')
+
+print("Uploaded Acumos files to Object Storage")
